8795f8e8796e1f33c64228df058da4624a3fbb60
Phil Owen <phillipsowen@outlook.com>
2020-01-13 16:32:32 -0500

    updates to include: functional inserts into the redis DB, use the chemical substance id in the lookup data as the index

--- src/ChemNormalization.py
+++ src/ChemNormalization.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 
 import redis
+
 from neo4j import GraphDatabase
 
 from rdkit import Chem
@@ -21,82 +22,109 @@ from rdkit.Chem.rdmolops import RemoveStereochemistry
 ##############
 class ChemNormalization:
     # Storage for the configuration params
-    config: json = None
+    _config: json = None
+
+    # storage for a debug limit on the number of chemical substance records processed
+    _debug_record_limit: str = ''
 
     def __init__(self):
-        self.config = self.get_config()
+        """ class constructor """
+        self._config = self.get_config()
+        self._debug_record_limit = self._config['debug_record_limit']
         pass
 
-    def load_redis(self):
-        try:
-            # get the grouped simplified SMILES
-            smiles_list: list = self.get_simplified_smiles_for_chemicals()
+    def load_redis(self) -> bool:
+        """ Loads two redis databases with simplified chemical substance SMILES data. """
+        # init the return
+        rv: bool = True
 
-            # get a connection to the redis instance for the (SMILE -> Simple SMILES) and (Simple SMILES -> [Similar SMILES, ..]) data
-            # smiles_to_simple_smiles_redis = self.get_redis(self.config, '0')
-            # simple_smiles_to_similar_smiles_redis = self.get_redis(self.config, '1')
+        try:
+            # get a connection to the redis instance for the (ID -> Simple SMILES) and (Simple SMILES -> [Similar SMILES, ..]) data
+            id_to_simple_smiles_redis = self.get_redis(self._config, 0)
+            simple_smiles_to_similar_smiles_redis = self.get_redis(self._config, 1)
 
             # get the pipelines for redis loading
-            # smiles_to_simple_smiles_redis_pipeline = smiles_to_simple_smiles_redis.pipeline()
-            # simple_smiles_to_similar_smiles_pipeline = simple_smiles_to_similar_smiles_redis.pipeline()
+            id_to_simple_smiles_pipeline = id_to_simple_smiles_redis.pipeline()
+            simple_smiles_to_similar_smiles_pipeline = simple_smiles_to_similar_smiles_redis.pipeline()
 
-            # start loading redis with the data
-            for el in smiles_list:
-                print(f'test {el}')
+            # get the grouped and simplified SMILES
+            df_gb: pd.DataFramGroupBy = self.get_simplified_smiles_for_chemicals()
 
-            # print(f'Dumping to simple smiles to similar smiles db ...')
-            # simple_smiles_to_similar_smiles_pipeline.execute()
+            # loop through each record and create the proper dict for redis insertion
+            for simplified_SMILES, similar_SMILES_group in df_gb:
+                # init the storage for the data members (ID, original SMILES, simplified SMILES
+                members = []
 
-            # print(f'Dumping to simple smiles to similar smiles db ...')
-            # simple_smiles_to_similar_smiles_pipeline.execute()
+                # for each row in the group
+                for index, row in similar_SMILES_group.iterrows():
+                    # save the chemical substance ID and simplified SMILES lookup record to the redis cache
+                    print(f'ID to simplified SMILES -> Chem ID: {row["chem_id"]}, Simplified SMILES: {simplified_SMILES}, <Original SMILES: {row["original_SMILES"]}>')
+                    id_to_simple_smiles_pipeline.set(row["chem_id"], f'{simplified_SMILES}')
 
+                    # save each element of the group to generate a list of similar SMILES
+                    members.append({'id:': row['chem_id'], 'original_smiles': row['original_SMILES']})
+
+                # create an object for all the member elements
+                similar_smiles = {'members': [member for member in members], 'simplified_SMILES': simplified_SMILES}
+
+                # convert the data object into json format
+                final = json.dumps(similar_smiles)
+
+                print(f'Simplified SMILES to similar SMILES list -> Simplified SMILES: {simplified_SMILES}, Similar SMILES list: [{final}]\n')
+                simple_smiles_to_similar_smiles_pipeline.set(simplified_SMILES, final)
+
+                # DEBUG: build up the record to insert and add it to a dict list for inspection
+                # term: dict = {'simplified_SMILES': simplified_SMILES, 'similar_SMILES': [original_SMILES for original_SMILES in similar_SMILES_group.original_SMILES]}
+                # smiles_list.append(term)
+
+            # print(f'Dumping to chemical substance id to lookup db ...')
+            id_to_simple_smiles_pipeline.execute()
+
+            # print(f'Dumping to simple SMILES to array of similar SMILES db ...')
+            simple_smiles_to_similar_smiles_pipeline.execute()
         except Exception as e:
-            print(f'Bad news {e}')
+            print(f'Exeception thrown: {e}')
+            rv = False
 
-        pass
+        # return to the caller
+        return rv
 
     @staticmethod
     def get_config() -> json:
-        """ Loads the configuration settings. """
+        """ Loads the configuration settings into a class object. """
 
         # get the location of the configuration file
         cname = os.path.join(os.path.dirname(__file__), '..', 'config.json')
 
         # open the config file
         with open(cname, 'r') as json_file:
-            # parse the contents of the configuration settingx
+            # parse the contents of the configuration settings file
             data = json.load(json_file)
 
         # return to the caller
         return data
 
     @staticmethod
-    def get_redis(config: json, dbid: str) -> object:
-        """ This method returns a connection to a redis instance """
+    def get_redis(config: json, db_id: int):
+        """ Returns a connection to a redis instance """
         return redis.StrictRedis(host=config['redis_host'],
                                  port=int(config['redis_port']),
-                                 db=dbid,
+                                 db=db_id,
                                  password=config['redis_password'])
 
-    def get_simplified_smiles_for_chemicals(self) -> list:
-        """ This method gets smiles for every chemical substance in the robokop neo4j graph database and creates a simplified SMILES from each.
+    def get_simplified_smiles_for_chemicals(self) -> pd.DataFrame:
+        """ This method gets SMILES for every chemical substance in the robokop neo4j graph database and creates a simplified SMILES from each.
             The simplified SMILES values will be used as a grouping mechanism and saved in the redis database.
         """
 
-        # init the dicts that will be used to load redis
-        terms: list = []
-
-        # Declare the columns in the output data frame
-        cols: list = ['similar_smiles', 'simplified_smiles']
-
-        # Create a target dataframe for the processed data
-        df = pd.DataFrame(columns=cols)
+        # Create a target data frame for the processed data
+        df = pd.DataFrame(columns=['chem_id', 'original_SMILES', 'simplified_SMILES'])
 
         try:
             # Create the query. This is of course robokop specific
-            # to return 2 that have the same simplified SMILES use this in the where clause ->  and (c.id="CHEBI:140593" or c.id="CHEBI:140451")
-            # to return 2 that have the exact same SMILES use this in the where clause ->   and (c.id="CHEBI:85764" or c.id="CHEBI:140773")
-            c_query: str = f'match (c:chemical_substance) where c.smiles is not NULL and c.smiles <> "" and c.smiles <> "**" and c.smiles <> "*" RETURN c.id, c.smiles order by c.smiles'  # limit 50000'''
+            # DEBUG: to return 2 that have the same simplified SMILES use this in the where clause ->  and (c.id="CHEBI:140593" or c.id="CHEBI:140451")
+            # DEBUG: to return 2 that have the exact same SMILES use this in the where clause ->   and (c.id="CHEBI:85764" or c.id="CHEBI:140773")
+            c_query: str = f'match (c:chemical_substance) where c.smiles is not NULL and c.smiles <> "" and c.smiles <> "**" and c.smiles <> "*" RETURN c.id, c.smiles order by c.smiles {self._debug_record_limit}'
 
             # execute the query
             records = self.run_neo4j_query(c_query)
@@ -110,13 +138,13 @@ class ChemNormalization:
                         molecule = Chem.MolFromSmiles(r['c.smiles'])
                     except Exception as e:
                         # alert the user there was an issue and continue
-                        print(f"Error - Exception trying to get a molecule for id: {r['c.id']} with smiles: {r['c.smiles']}, Execption {e}. Proceeding.")
+                        print(f"Error - Exception trying to get a molecule for chem id: {r['c.id']} with original SMILES: {r['c.smiles']}, Execption {e}. Proceeding.")
                         continue
 
                     # did we get the molecule
                     if molecule is None:
                         # Couldn't parse the molecule
-                        print(f"Error - Got an empty molecule for id: {r['c.id']} with smiles: {r['c.smiles']}. Proceeding.")
+                        print(f"Error - Got an empty molecule for chem id: {r['c.id']} with smiles: {r['c.smiles']}. Proceeding.")
                         continue
                     try:
                         # get the uncharged version of the largest fragment
@@ -126,36 +154,27 @@ class ChemNormalization:
                         RemoveStereochemistry(molecule_uncharged)
 
                         # get the simplified SMILES value
-                        simplified_smiles = Chem.MolToSmiles(molecule_uncharged)
+                        simplified_smiles: str = Chem.MolToSmiles(molecule_uncharged)
+
+                        record = {'simplified_SMILES': simplified_smiles, 'chem_id': r['c.id'], 'original_SMILES': r['c.smiles']}
 
                         # append the record to the data frame
-                        df = df.append({'similar_smiles': r['c.smiles'], 'simplified_smiles': simplified_smiles}, ignore_index=True)
+                        df = df.append(record, ignore_index=True)
                     except Exception as e:
                         # alert the user that something was discovered in the original graph record
-                        print(f"Error - Could not get a simplified SMILES for id: {r['c.id']}, SMILES: {r['c.smiles']}, Exception: {e}")
-
-                # remove all duplicates
-                df = df.drop_duplicates()
+                        print(f"Error - Could not get a simplified SMILES for chem id: {r['c.id']}, Original SMILES: {r['c.smiles']}, Exception: {e}")
 
                 # get the simplified SMILES in groups
-                grps = df.groupby('simplified_smiles')
-
-                # loop through each record and create the proper dict for redis insertion
-                for _simplified_smiles, similar_smiles_group in grps:
-                    # build up the record to insert
-                    term: dict = {'simplified_smiles': _simplified_smiles, 'similar_smiles': [_similar_smiles for _similar_smiles in similar_smiles_group.similar_smiles]}
-
-                    # add it to the dict
-                    terms.append(term)
+                df = df.set_index('simplified_SMILES').groupby('simplified_SMILES')
         except Exception as e:
             raise e
 
         # return to the caller
-        return terms
+        return df
 
     def get_driver(self) -> GraphDatabase.driver:
         # Gets a connection to the graph database
-        driver = GraphDatabase.driver(self.config['neo4j_uri'], auth=(self.config['neo4j_user'], self.config['neo4j_password']))
+        driver = GraphDatabase.driver(self._config['neo4j_uri'], auth=(self._config['neo4j_user'], self._config['neo4j_password']))
 
         # return to the caller
         return driver
--- src/load_redis.py
+++ src/load_redis.py
@@ -1,6 +1,15 @@
 from src.ChemNormalization import ChemNormalization
 
+
 if __name__ == '__main__':
+    # instantiate the class that does all the work
     cn = ChemNormalization()
 
-    cn.load_redis()
+    # call to load redis instances with normailized chemical substance data
+    success: bool = cn.load_redis()
+
+    # check tthe return
+    if not success:
+        print(f'Failed to load redis with chemical normalization data.')
+    else:
+        print(f'Success')
