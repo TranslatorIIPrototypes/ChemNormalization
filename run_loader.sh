7f2babb3df10a2e9b535d0cd717ec47fd2c56de2
Phil Owen <phillipsowen@outlook.com>
2020-01-14 16:15:33 -0500

    fixing script

--- run_loader.sh
+++ run_loader.sh
@@ -4,23 +4,21 @@ read -p "Have you entered your configuration parameters into config.json [y/n]? 
 if [ $answer = 'n' ]
 then
   echo "Please input your settings into config.json and try again. Exiting..."
-  exit 1
+else
+  echo "Activating virtual python environment..."
+
+  # Activate the virtual environment (linux)
+  source activate chemNodeVenv
+
+  read -p "Are you ready to start the loading [y/n]? " answer
+
+  if [ $answer = 'n' ]
+  then
+    echo "Loading aborted. Exiting..."
+  else
+    echo "Loading..."
+    # python src/load-redis.py
+  fi
 fi
 
-echo "Activating virtual python environment..."
-
-# Activate the virtual environment (linux)
-source activate chemNodeVenv
-
-# Once the configuration is completed, initiate the load.
-
-read -p "Are you ready to start the loading [y/n]? " answer
-
-if [ $answer = 'n' ]
-then
-  echo "Loading aborted. Exiting..."
-  exit 1
-fi
-
-python src/load-redis.py
 
