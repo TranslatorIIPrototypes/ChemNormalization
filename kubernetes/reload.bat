00ed72bf81f2e1a164d3463222632a11acbfc3f7
Phil Owen <phillipsowen@outlook.com>
2020-01-30 15:49:29 -0500

    adding https port

--- kubernetes/reload.bat
+++ kubernetes/reload.bat
@@ -1,7 +1,6 @@
-kubectl delete deployment cn-neo4j-kp-dep
-kubectl delete service cn-neo4j-kp-svc
-kubectl delete pvc --all
-kubectl delete pv --all
+kubectl delete -f .
 pause
+
 kubectl create -f cn-neo4jkp-kube-conf.yml
 kubectl apply -f cn-neo4jkp-persitence.yml
+minikube service cn-neo4j-kp-svc --url
